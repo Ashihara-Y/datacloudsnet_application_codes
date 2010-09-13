cb60953581e99ad565a621976814ef16379baa90
root <root@hurusato.org>
2010-09-14 08:13:33 +0900

    rails new dataclouds-3alpha-alpha

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dataclouds3alphaAlpha::Application.config.secret_token = '23642a7486a2cd920ebaae015619910108fdaae94e7845c885175e2d6bf1302c82886389c26ccd015dcbe88bf013a2365df8c2782a89a6b8ebc56cd34a65ce22'
